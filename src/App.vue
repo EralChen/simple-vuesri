bf3010eec34e55bbcc2030f996e64b1676d3c318
EralChen <eralchen@foxmail.com>
2024-06-07 13:54:56 +0800

    init: test layer

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,22 @@
 <script setup>
 import { VaTdtBasemap } from '../components/tdt-base-map'
 import { VaMapView } from '../components/map-view'
+import TestMediaLayer from './TestMediaLayer.vue'
+import TestWebTileLayer from './TestWebTileLayer.vue'
 
 </script>
 <template>
   <VaMapView :defaultOptions="defaultOptions">
+    <!-- 加载底图 -->
     <VaTdtBasemap :type="'vec_c'" />
+
+    <!-- media layer -->
+    <TestMediaLayer></TestMediaLayer>
+
+
+    <!-- tile layer -->
+    <!-- 希望 tile layer 叠加到  media layer 之上-->
+    <TestWebTileLayer></TestWebTileLayer>
 
 
   </VaMapView>
---
+++ src/TestWebTileLayer.vue
@@ -0,0 +1,9 @@
+<script lang="ts" setup>
+import { useView } from '@vuesri/core/composables'
+const view = useView()
+const map = view.map
+
+</script>
+<template>
+  <slot></slot>
+</template>
